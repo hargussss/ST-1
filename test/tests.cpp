999b8753e35da0c88dd171efbb90ade51d42b70b
hargussss <ivkarpich@edu.hse.ru>
2026-02-28 20:09:34 +0300

    Complete task 1

--- test/tests.cpp
+++ test/tests.cpp
@@ -4,14 +4,72 @@
 #include <cstdint>
 #include "alg.h"
 
-
-TEST(st1, sumPrime1) {
-  uint64_t res = sumPrime(2000000);
-  uint64_t expected = 142913828922;
-  EXPECT_EQ(expected, res);
+// checkPrime tests
+TEST(checkPrimeTest, zeroIsNotPrime) {
+  EXPECT_FALSE(checkPrime(0));
 }
-TEST(st1, sumPrime2) {
-  uint64_t res = sumPrime(10);
-  uint64_t expected = 17;
-  EXPECT_EQ(expected, res);
+
+TEST(checkPrimeTest, oneIsNotPrime) {
+  EXPECT_FALSE(checkPrime(1));
+}
+
+TEST(checkPrimeTest, twoIsPrime) {
+  EXPECT_TRUE(checkPrime(2));
+}
+
+TEST(checkPrimeTest, threeIsPrime) {
+  EXPECT_TRUE(checkPrime(3));
+}
+
+TEST(checkPrimeTest, fourIsNotPrime) {
+  EXPECT_FALSE(checkPrime(4));
+}
+
+TEST(checkPrimeTest, largeComposite) {
+  EXPECT_FALSE(checkPrime(100));
+}
+
+TEST(checkPrimeTest, largePrime) {
+  EXPECT_TRUE(checkPrime(7919));
+}
+
+// nPrime tests
+TEST(nPrimeTest, firstPrime) {
+  EXPECT_EQ(2, nPrime(1));
+}
+
+TEST(nPrimeTest, secondPrime) {
+  EXPECT_EQ(3, nPrime(2));
+}
+
+TEST(nPrimeTest, sixthPrime) {
+  EXPECT_EQ(13, nPrime(6));
+}
+
+// nextPrime tests
+TEST(nextPrimeTest, nextAfterFour) {
+  EXPECT_EQ(5, nextPrime(4));
+}
+
+TEST(nextPrimeTest, nextAfterPrime) {
+  EXPECT_EQ(13, nextPrime(11));
+}
+
+TEST(nextPrimeTest, nextAfterOne) {
+  EXPECT_EQ(2, nextPrime(1));
+}
+
+// sumPrime tests
+TEST(sumPrimeTest, sumBelow2) {
+  EXPECT_EQ(0u, sumPrime(2));
+}
+
+TEST(sumPrimeTest, sumBelow10) {
+  uint64_t expected = 17;  // 2+3+5+7
+  EXPECT_EQ(expected, sumPrime(10));
+}
+
+TEST(sumPrimeTest, sumBelow2000000) {
+  uint64_t expected = 142913828922;
+  EXPECT_EQ(expected, sumPrime(2000000));
 }
